f87a0ea5708565f1de9268c10f7519bf9c801244
telosma <duylxbk57@gmail.com>
2017-04-02 01:42:46 +0700

    cm home page 2

--- resources/views/user/home.blade.php
+++ resources/views/user/home.blade.php
@@ -4,7 +4,7 @@
     <div class="main-content">
         <div class="container-fluid">
             <div class="row">
-                <div class="col-md-2 col-sm-2">
+                <div class="col-md-2 col-sm-6">
                     <div class="nav-left">
                         <ul>
                             <li class="group-menu">
@@ -22,7 +22,7 @@
                                     <span>Loai van ban</span>
                                 </div>
                                 <ul class="menu">
-                                    <li class="active"><a>Hien phao</a></li>
+                                    <li><a>Hien phap</a></li>
                                     <li><a>Bo luat</a></li>
                                     <li><a>Phap lenh</a></li>
                                 </ul>
@@ -30,13 +30,143 @@
                         </ul>
                     </div>
                 </div>
-                <div class="col-md-8 col-sm-8">
+                <div class="col-md-7 col-sm-6">
                     <div class="body-content">
-
+                        <div class="list-law">
+                            <div class="panel panel-default item">
+                                <div class="panel-heading item-title">
+                                    <h3 class="panel-title">Van ban abc</h3>
+                                </div>
+                                <div class="panel-body item-body">
+                                    <div class="left col-sm-8">
+                                        <div class="row desc">
+                                            <p>
+                                                Quy định về tuyển dụng, sử dụng, bồi dưỡng đối với nhà giáo giáo dục nghề nghiệp
+                                            </p>
+                                        </div>
+                                        <div class="row">
+                                            <ul class="link">
+                                                <li>
+                                                    <div class="col-sm-3 source">
+                                                        <i class="fa fa-file-o"></i>
+                                                        <a href="#">Ban PDF</a>
+                                                    </div>
+                                                </li>
+                                                <li>
+                                                    <div class="col-sm-3 source">
+                                                        <i class="fa fa-file-o"></i>
+                                                        <a href="#">VB lien quan</a>
+                                                    </div>
+                                                </li>
+                                                <li>
+                                                    <div class="col-sm-3 source">
+                                                        <i class="fa fa-flash"></i>
+                                                        <a href="#">Thuoc tinh</a>
+                                                    </div>
+                                                </li>
+                                                <li>
+                                                    <div class="col-sm-2 source">
+                                                        <i class="fa fa-download"></i>
+                                                        <a href="#">Tai ve</a>
+                                                    </div>
+                                                </li>
+                                            </ul>
+                                        </div>
+                                    </div>
+                                    <div class="right col-sm-4">
+                                        <table class="table">
+                                            <tr class="law-date">
+                                                <td>Ban hành:</td>
+                                                <td>23/03/2017</td>
+                                            </tr>
+                                            <tr class="law-date">
+                                                <td>Hiệu lực:</td>
+                                                <td>07/05/2017</td>
+                                            </tr>
+                                            <tr class="law-effect">
+                                                <td>Trang thai</td>
+                                                <td>Chua co hieu luc</td>
+                                            </tr>
+                                        </table>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="panel panel-default item">
+                                <div class="panel-heading item-title">
+                                    <h3 class="panel-title">Van ban abc</h3>
+                                </div>
+                                <div class="panel-body">
+                                    <div class="col-sm-8 left">
+                                        <div class="row desc">
+                                            <p>
+                                                Ban hành Quy chế lập, thẩm tra, quyết định kế hoạch tài chính 05 năm địa phương, kế hoạch đầu tư công trung hạn 05 năm địa phương, kế hoạch tài chính - ngân sách nhà nước 03 năm địa phương, dự toán và phân bổ ngân sách địa phương, phê chuẩn quyết toán ngân sách địa phương hằng năm
+                                            </p>
+                                        </div>
+                                        <div class="row">
+                                            <ul class="link">
+                                                <li>
+                                                    <div class="col-sm-3 source">
+                                                        <i class="fa fa-file-o"></i>
+                                                        <a href="#">Ban PDF</a>
+                                                    </div>
+                                                </li>
+                                                <li>
+                                                    <div class="col-sm-3 source">
+                                                        <i class="fa fa-file-o"></i>
+                                                        <a href="#">VB lien quan</a>
+                                                    </div>
+                                                </li>
+                                                <li>
+                                                    <div class="col-sm-3 source">
+                                                        <i class="fa fa-flash"></i>
+                                                        <a href="#">Thuoc tinh</a>
+                                                    </div>
+                                                </li>
+                                                <li>
+                                                    <div class="col-sm-2 source">
+                                                        <i class="fa fa-download"></i>
+                                                        <a href="#">Tai ve</a>
+                                                    </div>
+                                                </li>
+                                            </ul>
+                                        </div>
+                                    </div>
+                                    <div class="right col-sm-4">
+                                        <table class="table">
+                                            <tr class="law-date">
+                                                <td>Ban hành:</td>
+                                                <td>23/03/2017</td>
+                                            </tr>
+                                            <tr class="law-date">
+                                                <td>Hiệu lực:</td>
+                                                <td>07/05/2017</td>
+                                            </tr>
+                                            <tr class="law-effect">
+                                                <td>Trang thai</td>
+                                                <td>Chua co hieu luc</td>
+                                            </tr>
+                                        </table>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
                 <div class="col-md-2 col-sm-2">
-                    <div class="nav-right"></div>
+                    <div class="nav-right">
+                        <ul>
+                            <li class="group-menu">
+                                <div class="menu-head">
+                                    <span>Van ban xem nhieu</span>
+                                </div>
+                                <ul class="menu">
+                                    <li><a>Thu tuong</a></li>
+                                    <li><a>Quoc hoi</a></li>
+                                    <li><a>Chu tich nuoc</a></li>
+                                </ul>
+                            </li>
+                        </ul>
+                    </div>
                 </div>
             </div>
         </div>
